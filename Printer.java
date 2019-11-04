11423aeddd68cc8e9ebec53180c6f4bcfc154103
Alex Wong <awong5@slb.com>
2019-11-04 12:16:56 -0600

    addd the enw branch

--- Printer.java
+++ Printer.java
@@ -1,14 +1,4 @@
 public class Printer {
-
-    public void PrintOnce() {
-        System.out.println("Hello, PrintOnce");
-    }
-
-    public void PrintTwice() {
-        System.out.println("Hello, PrintTwice");
-        System.out.println("Hello, PrintTwice");
-    }
-
     public void PrintN(int n) {
         for (int i = 0; i < n; i++) {
             System.out.println("Hello, PrintN");
--- p1.java
+++ p1.java
@@ -1,12 +1,6 @@
 public class p1 {
     public static void main(String[] args) {
         Printer myPrinter = new Printer();
-
-        System.out.println("Hello, World");
-        System.out.println("Hello, World2");
-        System.out.println("Hello, World3");
-        myPrinter.PrintOnce();
-        myPrinter.PrintTwice();
         myPrinter.PrintN(2);
     }
 
