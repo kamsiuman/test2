c75639924d47a3fd6d1e9ddf9110dba37c6e378c
Alex Wong <awong5@slb.com>
2019-11-04 11:58:29 -0600

    split file

---
+++ p1.java
@@ -0,0 +1,13 @@
+public class p1 {
+    public static void main(String[] args) {
+        Printer myPrinter = new Printer();
+
+        System.out.println("Hello, World");
+        System.out.println("Hello, World2");
+        System.out.println("Hello, World3");
+        myPrinter.PrintOnce();
+        myPrinter.PrintTwice();
+        myPrinter.PrintN(2);
+    }
+
+}
